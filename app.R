04738cfb5c033c3c34a08e50bc16292066030570
David Eichenberg <david.eichenber@gmx.de>
2020-10-30 20:57:05 +0100

    Added a tab for absolute changes

--- app.R
+++ app.R
@@ -7,50 +7,103 @@ winnersloosers<- readRDS("./datasets/winnersloosers.rds")
 winnersloosers$TaxonName<-as.character(winnersloosers$TaxonName)
 Encoding(winnersloosers$TaxonName)<- c("UTF-8")
 winnersloosers$TaxonName<- as.factor(as.character(winnersloosers$TaxonName))
-orderspecs<- winnersloosers$TaxonName[order(winnersloosers$perc_3_1,decreasing=T)]
+orderspecs<- winnersloosers$TaxonName[order(winnersloosers[,2],decreasing=T)]
 
 
+winnersloosers_abs<- readRDS("./datasets/winnersloosers_abs.rds")
+winnersloosers_abs$TaxonName<-as.character(winnersloosers_abs$TaxonName)
+Encoding(winnersloosers_abs$TaxonName)<- c("UTF-8")
+winnersloosers_abs$TaxonName<- as.factor(as.character(winnersloosers_abs$TaxonName))
+orderspecs_abs<- winnersloosers_abs$TaxonName[order(winnersloosers_abs[,2],decreasing=T)]
 
 
 winnersloosers$Species<- factor(winnersloosers$TaxonName,
-                                    levels=orderspecs)
+                                levels=orderspecs)
+
+
+winnersloosers_abs$Species<- factor(winnersloosers_abs$TaxonName,
+                                    levels=orderspecs_abs)
+
+
+row.names(winnersloosers_abs)<- seq(from=1,to=2140, by=1)
+
+row.names(winnersloosers)<- seq(from=1,to=2140, by=1)
 
 
 names(winnersloosers)[2]<- c("Change")
 winnersloosers<- winnersloosers[,c(4,2)]
+
+names(winnersloosers_abs)[2]<- c("Change")
+winnersloosers_abs<- winnersloosers_abs[,c(3,2)]
+
+
+
 winloosecols<- readRDS("./datasets/winloosecols.rds")
+winloosecols_abs<- readRDS("./datasets/winloosecols_abs.rds")
+
+
 
 info_winnersloosers<- readRDS("./datasets/info_winnersloosers.rds")
 info_winnersloosers$Species<- as.character(info_winnersloosers$Species)
+
+
+info_winnersloosers_abs<- readRDS("./datasets/info_winnersloosers_abs.rds")
+info_winnersloosers_abs$Species<- as.character(info_winnersloosers_abs$Species)
+
 
 Encoding(info_winnersloosers$Species)<- c("UTF-8")
 info_winnersloosers<- info_winnersloosers[which(info_winnersloosers$Species %in% winnersloosers$Species),]
 info_winnersloosers<-info_winnersloosers[order(info_winnersloosers$`Delta Period 3-1`,decreasing=T),]
 
 
+Encoding(info_winnersloosers_abs$Species)<- c("UTF-8")
+info_winnersloosers_abs<- info_winnersloosers_abs[which(info_winnersloosers_abs$Species %in% winnersloosers_abs$Species),]
+info_winnersloosers_abs<-info_winnersloosers_abs[order(info_winnersloosers_abs$`Delta Period 3-1`,decreasing=T),]
+
+
+
 ui <- fluidPage(
   fluidRow(
     tabsetPanel(type = "tabs",
                 id = "mean_or_sd_maps",
-                tabPanel("Winners and Losers in distributional change",
+                tabPanel("Winners and Losers in distributional change (relative)",
                          column(width = 12, class = "well",
-                        h4(div(HTML("Plot of changes in SOP<sub>Spec</sub> 
+                                h4(div(HTML("Plot of relative changes in SOP<sub>Spec</sub> [%] 
                                     as presented in Eichenberg <i>et al.</i> (2020)")
-                               )
-                           ),
-                        h5(div(HTML("Plot shows the difference in species occurrence [%] in Germany between 1960-1987 and 1996-2017. <br>
+                                )
+                                ),
+                                h5(div(HTML("Plot shows the difference in species occurrence [%] in Germany between 1960-1987 and 1996-2017. <br>
                                     Hover mouse over a bar to see species name and the respective Change; drag a rectagle to zoom in if needed")
-                               )
-                           ),
-                        plotlyOutput("plot", height = 400)
+                                )
+                                ),
+                                plotlyOutput("plot", height = 400)
                          ),
-                          column(width=12, 
-                                 h5(div(HTML("The table shows the occurrence values 
+                         column(width=12, 
+                                h5(div(HTML("The table shows the occurrence values 
                                               of the selected species in all three study 
-                                              periods and their changes between the periods [%]."))),
+                                              periods and their relative changes between the periods [%]."))),
                                 dataTableOutput("table")
-                            )
-                          ),
+                         )
+                ),
+                tabPanel("Winners and Losers in distributional change (absolute)",
+                         column(width = 12, class = "well",
+                                h4(div(HTML("Plot of absolute changes in SOP<sub>Spec</sub>  
+                                    as presented in Eichenberg <i>et al.</i> (2020)")
+                                )
+                                ),
+                                h5(div(HTML("Plot shows the absolute differences in species occurrence in Germany between 1960-1987 and 1996-2017. <br>
+                                    Hover mouse over a bar to see species name and the respective Change; drag a rectagle to zoom in if needed")
+                                )
+                                ),
+                                plotlyOutput("plot2", height = 400)
+                         ),
+                         column(width=12, 
+                                h5(div(HTML("The table shows the occurrence values 
+                                              of the selected species in all three study 
+                                              periods and their absolute changes between the periods."))),
+                                dataTableOutput("table2")
+                         )
+                ),
                 tabPanel("Methodological information",
                          h3("Data compilation"),
                          br(),
@@ -159,39 +212,62 @@ ui <- fluidPage(
                               FlorKart dataset that were identified to be well-sampled by previous research. 
                               Therefore, changes in SOP<sub>Grid</sub> can be interpreted as meaningfully 
                               representing relative changes between grid-cells and timesteps.")
-                              )
-                        )
-                    )
-      )
+                )
+    )
+  )
+)
 
 
 
 server <- function(input, output) {
-   
-  output$plot <- renderPlotly({
   
+  output$plot <- renderPlotly({
+    
     p<- ggplot(data=winnersloosers, aes(x=Species, y=Change, col=Change, text=paste0("Change [%]: ",Change))) + 
-        geom_hline(yintercept=0, col="black") + geom_bar(stat="identity",width = 1)+
-        scale_color_gradientn(colors=winloosecols(2145),name="Change [%]")+
-        theme(axis.text.x = element_text(angle=45, size=3, vjust = 0.5, hjust=0),
-              panel.background = element_rect(fill="gray90")) + 
-        xlab("Species") + ylab("Change in SOP [%]")
+      geom_hline(yintercept=0, col="black") + geom_bar(stat="identity",width = 1)+
+      scale_color_gradientn(colors=winloosecols(2145),name="Change [%]")+
+      theme(axis.text.x = element_text(angle=45, size=3, vjust = 0.5, hjust=0),
+            panel.background = element_rect(fill="gray90")) + 
+      xlab("Species") + ylab("Change in SOP [%]")
     #plotly:: style(p, text=winnersloosers$Change)
     ggplotly(p, tooltip = c("x","text")) 
-    }
+  }
   )
   
   species_choice<- reactive({
     d<- event_data(event="plotly_hover")$x
-    }
-    )
+  }
+  )
   
   output$table<-renderDataTable({
     if(is.null(species_choice())){ info_winnersloosers} else{
-    info_winnersloosers[species_choice(),]}
-    }
-    )
-               
+      info_winnersloosers[species_choice(),]}
+  }
+  )
+  
+  output$plot2 <- renderPlotly({
+    
+    p<- ggplot(data=winnersloosers_abs, aes(x=Species, y=Change, col=Change, text=paste0("Change [abs]: ",Change))) + 
+      geom_hline(yintercept=0, col="black") + geom_bar(stat="identity",width = 1)+
+      scale_color_gradientn(colors=rev(winloosecols_abs(2140)),name="Change [abs]")+
+      theme(axis.text.x = element_text(angle=45, size=3, vjust = 0.5, hjust=0),
+            panel.background = element_rect(fill="gray90")) + 
+      xlab("Species") + ylab("Change in SOP [absolute]")
+    #plotly:: style(p, text=winnersloosers$Change)
+    ggplotly(p, tooltip = c("x","text")) 
+  }
+  )
+  
+  species_choice<- reactive({
+    d<- event_data(event="plotly_hover")$x
+  }
+  )
+  
+  output$table2<-renderDataTable({
+    if(is.null(species_choice())){ info_winnersloosers_abs} else{
+      info_winnersloosers_abs[species_choice(),]}
+  }
+  )          
 }
 
 
